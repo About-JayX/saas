4697c557c654b4378238af78fa832a856b6836b3
little-jay-ovo <3334354196@qq.com>
2022-06-08 00:23:12 +0800

    完成登录页面布局/添加表单效验规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,22 +5,22 @@
       :model="loginForm"
       :rules="loginRules"
       class="login-form"
-      auto-complete="on"
-      label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">
+          <img src="~@/assets/common/login-logo.png" alt="" />
+        </h3>
       </div>
 
-      <el-form-item prop="username">
+      <el-form-item prop="moblie">
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
         <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
+          ref="moblie"
+          v-model="loginForm.moblie"
+          placeholder="用户名"
+          name="moblie"
           type="text"
           tabindex="1"
           auto-complete="on"
@@ -36,7 +36,7 @@
           ref="password"
           v-model="loginForm.password"
           :type="passwordType"
-          placeholder="Password"
+          placeholder="密码"
           name="password"
           tabindex="2"
           auto-complete="on"
@@ -52,61 +52,53 @@
       <el-button
         :loading="loading"
         type="primary"
-        style="width: 100%; margin-bottom: 30px"
+        style="
+          width: 100%;
+          margin-bottom: 30px;
+          background-color: #407ffe;
+          height: 64px;
+          font-size: 24px;
+        "
         @click.native.prevent="handleLogin"
-        >登陆</el-button
+        >登录</el-button
       >
     </el-form>
   </div>
 </template>
 
 <script>
-import { validUsername } from "@/utils/validate";
-
 export default {
   name: "Login",
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error("Please enter the correct user name"));
-      } else {
-        callback();
-      }
-    };
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error("The password can not be less than 6 digits"));
-      } else {
-        callback();
-      }
-    };
     return {
       loginForm: {
-        username: "admin",
-        password: "111111",
+        moblie: "13800000002",
+        password: "123456",
       },
+      // 效验表单规则
       loginRules: {
-        username: [
-          { required: true, trigger: "blur", validator: validateUsername },
+        // 效验手机号
+        moblie: [
+          { required: true, trigger: "blur", message: "请输入手机号" },
+          {
+            pattern:
+              /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+            trigger: "blur",
+            message: "输入的手机号不合法！",
+          },
         ],
+        // 效验密码
         password: [
-          { required: true, trigger: "blur", validator: validatePassword },
+          { required: true, trigger: "blur", message: "请输入密码" },
+          { min: 6, max: 15, message: "输入的密码不合法", trigger: "blur" },
         ],
       },
       loading: false,
       passwordType: "password",
-      redirect: undefined,
     };
   },
-  watch: {
-    $route: {
-      handler: function (route) {
-        this.redirect = route.query && route.query.redirect;
-      },
-      immediate: true,
-    },
-  },
   methods: {
+    // 显示隐藏密码框
     showPwd() {
       if (this.passwordType === "password") {
         this.passwordType = "";
@@ -117,21 +109,12 @@ export default {
         this.$refs.password.focus();
       });
     },
+    // 全局表单效验
     handleLogin() {
-      this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          this.loading = true;
-          this.$store
-            .dispatch("user/login", this.loginForm)
-            .then(() => {
-              this.$router.push({ path: this.redirect || "/" });
-              this.loading = false;
-            })
-            .catch(() => {
-              this.loading = false;
-            });
+      this.$refs.loginForm.validate((v) => {
+        if (v) {
+          // 发送请求获取token
         } else {
-          console.log("error submit!!");
           return false;
         }
       });
@@ -167,9 +150,9 @@ $cursor: #fff;
       -webkit-appearance: none;
       border-radius: 0px;
       padding: 12px 5px 12px 15px;
-      color: $light_gray;
+      color: #89b0fe;
       height: 47px;
-      caret-color: $cursor;
+      caret-color: #89b0fe;
 
       &:-webkit-autofill {
         box-shadow: 0 0 0px 1000px $bg inset !important;
@@ -180,7 +163,7 @@ $cursor: #fff;
 
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    background: #d4e5ff;
     border-radius: 5px;
     color: #454545;
   }
@@ -195,7 +178,7 @@ $light_gray: #eee;
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  background: url("~@/assets/common/login.jpg") no-repeat center/cover;
   overflow: hidden;
 
   .login-form {
@@ -215,15 +198,8 @@ $light_gray: #eee;
   }
 
   .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
+    vertical-align: middle;
+    margin: 0 auto 40px;
   }
 
   .show-pwd {
